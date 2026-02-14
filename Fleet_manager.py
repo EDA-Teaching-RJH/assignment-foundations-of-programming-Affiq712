5b0b82c92a3949c83fa3ba580093a8670ef8ff76
Affiq712 <aemo2@kent.ac.uk>
2026-02-14 12:06:01 +0000

    def int_database

--- Fleet_manager.py
+++ Fleet_manager.py
@@ -1,10 +1,10 @@
 def main():
-    name, rank, division , ids = int_databse()
+    name, rank, division, ids = int_database()
     while True:
         display_menu()
         option = input("Select an option: ")
         if option == "1":
-            add_member(name, rank, divsion, ids)
+            add_member(name, rank, division, ids)
         elif option == "2":
             remove_member(name, rank, division, ids)
         elif option == "3":
@@ -24,6 +24,9 @@ def main():
             break
         else:
             print("Invalid option. Please try again.")
+
+        def int_database():
+            name = ()
         
 
         def display_menu():
@@ -37,3 +40,10 @@ def main():
             print("7. Calculate Payroll")
             print("8. Count Officers")
             print("9. Exit")
+
+        def int_database():
+            name = ("William Riker", "Pavel Chekov", "Deanna Troi", "Data", "Beverly Crusher")
+            rank = ("Commander", "Captain", "Lieutenant Commander", "Lieutenant Commander", "Commander")
+            division = ("Operations", "Science", "Command", "Engineering", "Medical")
+            ids = [1001, 1002, 1003, 1004, 1005]
+            return name, rank, division, ids
