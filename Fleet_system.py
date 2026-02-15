5138803809500c0370cd438faf4664768d5449b6
Affiq712 <aemo2@kent.ac.uk>
2026-02-15 02:41:01 +0000

    elif choice 6

--- Fleet_system.py
+++ Fleet_system.py
@@ -10,5 +10,9 @@ elif choice == "2":
     break
 elif choice == "3":
     update_rank(name, rank, division, ids)
+    print("Rank updated.")
+elif choice == "4":
     display_roster(name, rank, division, ids)
+elif choice == "5":
     search_crew(name, rank, division, ids)
+elif choice == "6":
