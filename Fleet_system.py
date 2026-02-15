514c2e2ef0e24c3ed2b4311161c984aa24edb150
Affiq712 <aemo2@kent.ac.uk>
2026-02-15 02:28:34 +0000

    elif choice

--- Fleet_system.py
+++ Fleet_system.py
@@ -6,6 +6,9 @@ def main():
     add_member(name, rank, division, ids)
 elif choice == "2":
     remove_member(name, rank, division, ids)
+    print("Member has been removed.")
+    break
+elif choice == "3":
     update_rank(name, rank, division, ids)
     display_roster(name, rank, division, ids)
     search_crew(name, rank, division, ids)
