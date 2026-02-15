c3256daaef090fe321bae72c74d5259bc5cd8da0
Affiq712 <aemo2@kent.ac.uk>
2026-02-15 11:08:07 +0000

    elif option == 3 update_rank

--- Fleet_manager.py
+++ Fleet_manager.py
@@ -5,3 +5,11 @@ def main():
         option = input("Select an option: ")
         if option == "1":
             add_member(name, rank, division, ids)
+            print("Welcome to the fleet")
+        elif option == "2":
+            remove_member(name, rank, division, ids)
+            print("Exiting the fleet, have a great day!")
+            break
+        elif option == "3":
+            update_rank(name, rank, division, ids)
+            print("Rank has been successfully updated.")
