2cc29107c1236b373de2404687c500304cf6c995
Affiq712 <aemo2@kent.ac.uk>
2026-02-15 02:19:40 +0000

    While True

--- Fleet_system.py
+++ Fleet_system.py
@@ -1,3 +1,9 @@
 def main():
-    int_database()
-    display_menu()
+    name, rank, division, ids = int_database()
+    while True:
+        choice = display_menu()
+    add_member(name, rank, division, ids)
+    remove_member(name, rank, division, ids)
+    update_rank(name, rank, division, ids)
+    display_roster(name, rank, division, ids)
+    search_crew(name, rank, division, ids)
