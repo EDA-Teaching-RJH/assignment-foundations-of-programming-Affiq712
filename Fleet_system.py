b434cca2c177cd2b8778e5d0c7b55f18e25c5218
Affiq712 <aemo2@kent.ac.uk>
2026-02-15 02:23:54 +0000

    Elif choice 2

--- Fleet_system.py
+++ Fleet_system.py
@@ -1,8 +1,10 @@
 def main():
     name, rank, division, ids = int_database()
     while True:
-        choice = display_menu()
+        choice = display_menu(name, rank, division, ids)
+        if choice == "1":
     add_member(name, rank, division, ids)
+elif choice == "2":
     remove_member(name, rank, division, ids)
     update_rank(name, rank, division, ids)
     display_roster(name, rank, division, ids)
