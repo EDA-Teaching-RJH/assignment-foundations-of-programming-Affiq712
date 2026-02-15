c4ee23161b125ecb928e562890dc2f8620897147
Affiq712 <aemo2@kent.ac.uk>
2026-02-15 10:58:47 +0000

    if option == 1: add_member

--- Fleet_manager.py
+++ Fleet_manager.py
@@ -1,1 +1,7 @@
 def main():
+    name, rank, division, ids = int_database()
+    while True:
+        display_menu()
+        option = input("Select an option: ")
+        if option == "1":
+            add_member(name, rank, division, ids)
