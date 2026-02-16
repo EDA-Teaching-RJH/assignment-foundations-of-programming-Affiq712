95d4727ca1a52810e0aac9f76e89a2121af666f8
Affiq712 <aemo2@kent.ac.uk>
2026-02-16 15:39:47 +0000

    else: print unable to find crew member

--- old_system.py
+++ old_system.py
@@ -52,6 +52,8 @@ def run_system_monolith():
             r.pop(idx)
             d.pop(idx)
             print("Removed.")
+        else:
+            print("unable to find crew member.")
             
         elif opt == "4":
             print("Analyzing...")
