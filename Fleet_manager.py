32d485daccbfb74355a6b6f646992fa1ea52643d
Affiq712 <aemo2@kent.ac.uk>
2026-02-16 11:12:30 +0000

    opt == 1, line 28 fixed

--- Fleet_manager.py
+++ Fleet_manager.py
@@ -1,97 +1,97 @@
 def int_database():
-            name = ["William Riker", "Deanna Troi", "Data", "Spock", "Paul chekov"]
-            rank = ["Commander", "Lieutenant Commander", "captain", "Captain", "lieutenant"]
-            division = ["Command", "Operations", "science", "Engineering","Security"]
-            ids = [1, 2, 3, 4, 5]
-            return name, rank, division, ids
+    name = ["William Riker", "Deanna Troi", "Data", "Spock", "Paul chekov"]
+    rank = ["Commander", "Lieutenant Commander", "Captain", "Captain", "Lieutenant"]
+    division = ["Command", "Operations", "Science", "Engineering","Security"]
+    ids = [1, 2, 3, 4, 5]
+    return name, rank, division, ids
 def display_menu():
-            print("Welcome to fleet management system")
-            print("1. Add a crew member")
-            print("2. Remove a crew member")
-            print("3. update rank of crew member")
-            print("4. Display roster")
-            print("5. Search for crew member")
-            print("6. Filter division")
-            print("7. Calculate payroll")
-            print("8. Count officers by rank")
+    print("Welcome to fleet management system")
+    print("1. Add a crew member")
+    print("2. Remove a crew member")
+    print("3. update rank of crew member")
+    print("4. Display roster")
+    print("5. Search for crew member")
+    print("6. Filter division")
+    print("7. Calculate payroll")
+    print("8. Count officers by rank")
 def add_member(name, rank, division, ids):
-            new_name = input("Enter the name of the new member: ")
-            new_rank = input("Enter rank of new member: ")
-            new_division = input("Enter division of new member: ")
-            new_ids = int(input("Enter ID of the new member: "))
+    new_name = input("Enter the name of the new member: ")
+    new_rank = input("Enter rank of new member: ")
+    new_division = input("Enter division of new member: ")
+    new_ids = int(input("Enter ID of the new member: "))
 
-            name.append(new_name)
-            rank.append(new_rank)
-            division.append(new_division)
-            ids.append(new_ids)
+    name.append(new_name)
+    rank.append(new_rank)
+    division.append(new_division)
+    ids.append(new_ids)
 
 def remove_member(name, rank, division, ids):
-            remove_name = input("Enter name of member to remove: ")
-            if remove_name in name:
-                   index = name.index(remove_name)
-                   name.pop(index)
-                   rank.pop(index)
-                   division.pop(index)
-                   ids.pop(index)
-            else:
-                   print("Member not identified. Please try again.")
+    remove_name = input("Enter name of member to remove: ")
+    if remove_name in name:
+     index = name.index(remove_name)
+     name.pop(index)
+     rank.pop(index)
+     division.pop(index)
+     ids.pop(index)
+    else:
+     print("Member not identified. Please try again.")
      
 
 def update_rank(name, rank, ids):
     update_name = input("Enter new name of member to update rank:")
     if update_name in name:
-        new_rank = input("Enter new rank of member:")
-        new_ids  = int(input("Enter ID of member: "))
-        index = name.index(update_name)
-        rank[index] = new_rank
-        ids[index] = new_ids
+     new_rank = input("Enter new rank of member:")
+     new_ids  = int(input("Enter ID of member: "))
+     index = name.index(update_name)
+     rank[index] = new_rank
+     ids[index] = new_ids
     else:
         print("Member not identified.")
         
 def display_roster(name, rank, division, ids):
-            print("name", "rank", "division", "ids")
-            for i in range(len(name)): 
-                print(name[i], rank[i], division[i], ids[i])
+        print("name", "rank", "division", "ids")
+        for i in range(len(name)): 
+           print(name[i], rank[i], division[i], ids[i])
 
 def search_crew(name, rank, division, ids):
-            search = input("Enter name of member to search: ")
-            if search in name:
-                index = name.index
-                print("name:", name[index])
-                print("rank:", rank[index])
-                print("Division:", division[index])
-                print("ID:", ids[index])
-            else:
-                print("Member not found. Please try again.")
+    search = input("Enter name of member to search: ")
+    if search in name:
+        index = name.index(search)
+        print("name:", name[index])
+        print("rank:", rank[index])
+        print("Division:", division[index])
+        print("ID:", ids[index])
+    else:
+        print("Member not found. Please try again.")
         
 def filter_by_division(name, division):
-            name = input("Enter name of member to filter by divisions(command, operations, science, engineering, secuirty) ")
-            division = input("Enter division (command, operations, science, engineering, security):")
-            print("name", "division")
-            for i in range(len(name)):
-                if division[i].lower() == division.lower():
-                    print(name[i], division[i])
+    search_div = input("Enter name of division to filter: ").lower()
+    division = input("Enter division (command, operations, science, engineering, security):")
+    print("name", "division")
+    for i in range(len(name)):
+        if division[i].lower() == division.lower():
+            print(name[i], division[i])
 
 def calculate_payroll(rank):
-            rank = input("Enter rank to calculate payroll:")
-            if rank == "commander":
-                print("pay for commander is £100,000 ")
-            elif rank == "lieutenant commander":
-                print("pay for lieutenant commander is £90,000")
-            elif rank == "captain":
-                print("pay for captain is £80,000")
-            elif rank == "lieutenant":
-                print("pay for lieutenant is £60,000")
-            else:
-                print("rank not identified. Please try again.")
+    rank = input("Enter rank to calculate payroll:").lower()
+    if rank == "commander":
+        print("pay for commander is £100,000 ")
+    elif rank == "lieutenant commander":
+        print("pay for lieutenant commander is £90,000")
+    elif rank == "captain":
+        print("pay for captain is £80,000")
+    elif rank == "lieutenant":
+        print("pay for lieutenant is £60,000")
+    else:
+        print("rank not identified. Please try again.")
 
-def count_officers(rank):
-                rank = input("input rank to count the officers:")
-                count = 0
-                for i in range(len(rank)):
-                    if rank [i] == rank:
-                        count += 1
-                print("There are", count, update_rank, "officers in fleet.")
+def count_officers(rank_list):
+    rank = input("input rank to count the officers:")
+    count = 0
+    for i in range(len(rank)):
+         if rank [i] == rank:
+            count += 1
+    print("There are", count, update_rank, "officers in fleet.")
 
             
 def main():
